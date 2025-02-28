c348a643f0a2d76b724b1e962314d34cf1da509b
Stoyanova, Maria H. <149206845+MariaStoyanovva@users.noreply.github.com>
2025-02-28 08:54:54 +0200

    Create MovieCollection.java

---
+++ MovieCollection.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MovieCollection {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Movie{
+    private String title;
+    public Movie(String title){
+        this.title=title;
+    }
+}
+
+/*
+MOVIE COLLECTION MANAGER
+[add movie][sort (dropdown !-Z/genre)]
+ */
